e37e6ae6754103e214419ecc3582e05c604e9781
yunusdvp <yunusemre7033@gmail.com>
2022-09-16 10:04:55 +0300

    Initial Commit

---
+++ TurkPlus+/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TurkPlus+
+//
+//  Created by Yunus Emre ÖZŞAHİN on 16.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
